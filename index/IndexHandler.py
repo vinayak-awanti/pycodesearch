3692c6e230f355393e703ef9b31b2116ef6f8c66
Varun Vora <varunvora@live.com>
2018-10-18 16:20:35 +0530

    added cpython (80mb). changed index values to set. builds index for cpython 10 seconds faster. pep8

--- index/IndexHandler.py
+++ index/IndexHandler.py
@@ -6,44 +6,53 @@ from datetime import datetime
 
 logging.basicConfig(level="INFO")
 
-class IndexHandler:
-  def __init__(self):
-    self.index_file = "index.pkl"
-  
-  # builds the index for the contents of a directory
-  def build(self, dir):
-    self.index = defaultdict(list)
-    self.files = []
-    self.fileid = 0
-    logging.info("index creation starting")
-    start_time = datetime.now()
-    for root, _, files in os.walk(dir):
-      for file in files:
-        file = os.path.join(root, file)
-        self.add_file(file)
-        self.files.append(file)
-    logging.info("index created %s", str(datetime.now() - start_time))
-    with open(self.index_file, "wb") as fp:
-      pickle.dump((self.index, self.files), fp)
-    logging.info("index written to disk %s", str(datetime.now() - start_time))
-  
-  # adds contents of given file to the trigram index
-  def add_file(self, file):
-    with open(file, "r") as fp:
-      doc = fp.read()
-    for i in range(len(doc) - 2):
-      tri = doc[i:i+3]
-      if len(self.index[tri]) == 0 or self.index[tri][-1] < self.fileid:
-        self.index[tri].append(self.fileid)
-    self.fileid += 1
 
-  # returns the index
-  def read(self):
-    with open(self.index_file, "rb") as fp:
-      index = pickle.load(fp)
-    return index
+class IndexHandler:
+
+    def __init__(self):
+        self.index_file = "index.pkl"
+        self.index = defaultdict(set)
+        self.files = []
+        self.fileid = 0
+
+    # builds the index for the contents of a directory
+    def build(self, root):
+        logging.info("index creation starting")
+        start_time = datetime.now()
+        for root, _, files in os.walk(root):
+            for file in files:
+                file = os.path.join(root, file)
+                self.add_file(file)
+                self.files.append(file)
+        logging.info("index created %s", str(datetime.now() - start_time))
+        with open(self.index_file, "wb") as fp:
+            pickle.dump((self.index, self.files), fp)
+        logging.info("index written to disk %s", str(datetime.now() - start_time))
+
+    # adds contents of given file to the trigram index
+    def add_file(self, file):
+        try:
+            with open(file, "r") as fp:
+                doc = fp.read()
+            prev = doc[:3]
+            if len(prev) == 3:
+                self.index[prev].add(self.fileid)
+            for char in doc[3:]:
+                prev = prev[1:] + char
+                self.index[prev].add(self.fileid)
+            self.fileid += 1
+        except UnicodeDecodeError:
+            # logging.info("Couldn't read", file)
+            pass
+
+    # returns the index
+    def read(self):
+        with open(self.index_file, "rb") as fp:
+            index = pickle.load(fp)
+        return index
+
 
 if __name__ == "__main__":
-  index_handler = IndexHandler()
-  index_handler.build("gcs")
-  logging.info(index_handler.read())
+    index_handler = IndexHandler()
+    index_handler.build("cpython-master")
+    # logging.info(index_handler.read())
--- index/QueryHandler.py
+++ index/QueryHandler.py
@@ -1,66 +1,74 @@
-from IndexHandler import IndexHandler
+from index.IndexHandler import IndexHandler
 import logging
+from time import time
 
 logging.basicConfig(level="INFO")
 
+
 class QueryHandler:
-  def __init__(self):
-    index_handler = IndexHandler()
-    self.index, self.files = index_handler.read()
+    def __init__(self):
+        index_handler = IndexHandler()
+        self.index, self.files = index_handler.read()
 
-  # returns intersection of posting lists of the given trigrams
-  def list_and(self, lst1, tri):
-    i, j = 0, 0
-    lst2, lst3 = self.index[tri], []
-    n, m = len(lst1), len(lst2)
-    while i < n and j < m:
-      if lst1[i] < lst2[j]:
-        i += 1
-      elif lst1[i] > lst2[j]:
-        j += 1
-      else:
-        lst3.append(lst1[i])
-        i += 1
-        j += 1
-    return lst3
+    # returns intersection of posting lists of the given trigrams
+    def list_and(self, lst1, tri):
+        #TODO return lst1 & self.index[tri] is faster
+        i, j = 0, 0
+        lst2, lst3 = list(self.index[tri]), []
+        n, m = len(lst1), len(lst2)
+        while i < n and j < m:
+            if lst1[i] < lst2[j]:
+                i += 1
+            elif lst1[i] > lst2[j]:
+                j += 1
+            else:
+                lst3.append(lst1[i])
+                i += 1
+                j += 1
+        return lst3
 
-  # returns union of posting lists of the given trigrams
-  def list_or(self, lst1, tri):
-    i, j = 0, 0
-    n, lst2, lst3 = len(lst1), self.index[tri], []
-    m = len(lst2)
-    while(i < n and j < m):
-      x, y = lst1[i], lst2[j]
-      if(x == y):
-        lst3.append(x)
-        i += 1
-        j += 1
-      elif(x < y):
-        lst3.append(x)
-        i += 1
-      else:
-        lst3.append(y)
-        j += 1
-    if(i < n):
-      lst3.extend(lst1[i:])
-    if(j < m):
-      lst3.append(lst2[j:])
-    return lst3  
+    def list_or(self, lst1, tri):
+        #TODO return lst1 | self.index[tri] is faster
+        # returns union of posting lists of the given trigrams
+        i, j = 0, 0
+        n, lst2, lst3 = len(list(lst1)), list(self.index[tri]), []
+        m = len(lst2)
+        while i < n and j < m:
+            x, y = lst1[i], lst2[j]
+            if x == y:
+                lst3.append(x)
+                i += 1
+                j += 1
+            elif x < y:
+                lst3.append(x)
+                i += 1
+            else:
+                lst3.append(y)
+                j += 1
+        if i < n:
+            lst3.extend(lst1[i:])
+        if j < m:
+            lst3.append(lst2[j:])
+        return lst3
 
-  # returns candiate filenames
-  def query(self, tri_query):
-    lst = self.index[tri_query[0]]
-    for i in range(1,len(tri_query),2):
-      if(tri_query[i] == '&'):
-        lst = self.list_and(lst, tri_query[i+1])
-      else:
-        lst = self.list_or(lst, tri_query[i+1])
-    filenames = list(map(lambda x : self.files[x], lst))
-    return filenames
+    def query(self, tri_query):
+        # returns candiate filenames
+        lst = self.index[tri_query[0]]
+        for i in range(1, len(tri_query), 2):
+            if tri_query[i] == '&':
+                lst = self.list_and(lst, tri_query[i + 1])
+            else:
+                lst = self.list_or(lst, tri_query[i + 1])
+        filenames = list(map(lambda x: self.files[x], lst))
+        return filenames
 
 
 if __name__ == '__main__':
-  query_handler = QueryHandler()
-  logging.info(query_handler.list_or([0, 2], 'oog'))
-  logging.info(query_handler.list_and([0, 2], 'Goo'))
-  logging.info(query_handler.query(['Goo']))
+    start = time()
+    query_handler = QueryHandler()
+    logging.info(query_handler.list_or([0, 2], 'oog'))
+    logging.info(query_handler.list_and([0, 2], 'Goo'))
+    logging.info(time()-start)
+    quit()
+    while True:
+        logging.info(query_handler.query([input()]))
