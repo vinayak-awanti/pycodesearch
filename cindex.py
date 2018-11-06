c5a1b0621f276579b0f7d78784d07737dab3fed0
VarunV-007 <97v.varun@gmail.com>
2018-11-06 17:53:49 +0530

    put everything together; changed query handler;

---
+++ cindex.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from index import IndexHandler
+
+if len(sys.argv) != 2:
+    print("usage: python cindex.py path")
+    quit()
+
+index_handler = IndexHandler.IndexHandler()
+index_handler.build(os.path.abspath(sys.argv[1]))
+    
